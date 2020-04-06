2c3a4ace0362cb3ef71428aa770fd212c6686f6f
Will Mitchell <ChangedNameTo@users.noreply.github.com>
2020-04-06 09:30:54 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Input, Container} from 'semantic-ui-react'
+
+const ConsoleLog = ({ children }) => {
+  console.log(children);
+  return false;
+};
+
+class FFLogsInput extends Input {
+  constructor(props) {
+    super(props);
+    this.state = {
+      link: null,
+    }
+  }
+
+  checkAndGo() {
+    this.state.link = i;
+    return (i === 'https://www.fflogs.com/user/reports-list/237412/');
+  }
+
+  render() {
+    return (
+      <div>
+        <ConsoleLog>{this.state.link}</ConsoleLog>
+        <Input
+          placeholder = 'Paste your FFLogs Profile'
+          onChange = {this.checkAndGo()}
+        />
+      </div>
+    );
+  }
+}
+
+class PartyCheck extends React.Component {
+  render() {
+    return (
+      <Container>
+        <div>
+          <FFLogsInput />
+        </div>
+      </Container>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PartyCheck />
   );
 }
 
