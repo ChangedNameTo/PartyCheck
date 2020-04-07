a8849fec5384d471ba8de172699869eba378a375
Will Mitchell <ChangedNameTo@users.noreply.github.com>
2020-04-07 00:01:26 -0400

    Basic working table design.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import {Input, Container, Menu, Segment, Table} from 'semantic-ui-react'
-import rp from "request-promise";
+var format = require('string-template');
+const axios = require('axios').default;
+require('dotenv').config();
 
-class PartyCheckMenu extends Menu {
-  render() {
-    const {activeItem} = this.state
-
-    return (
-      <Menu>
-        <Container>
-          <Menu.Item
-            name='Home'
-            active={activeItem === 'home'}
-            onClick={this.props.onClick}
-          />
-        </Container>
-      </Menu>
-    );
-  }
+function PartyCheckMenu(props) {
+  return (
+    <Menu>
+      <Container>
+        <Menu.Item
+          name='Home'
+          onClick={props.onClick}
+        />
+      </Container>
+    </Menu>
+  );
 }
 
 class Header extends React.Component {
@@ -30,33 +27,28 @@ class Header extends React.Component {
   }
 }
 
-class FFLogsInput extends Input {
-  render() {
-    return (
-      <div>
-        <Input
-          fluid
-          icon = 'search'
-          iconPosition = 'left'
-          name = "fflogslink"
-          placeholder = 'Paste your FFLogs Profile'
-          onChange = {this.props.onChange}
-        />
-      </div>
-    );
-  }
+function FFLogsInput(props) {
+  return (
+    <div>
+      <Input
+        fluid
+        icon = 'search'
+        iconPosition = 'left'
+        name = "fflogslink"
+        placeholder = 'Enter your FFLogs Username'
+        onChange = {props.onChange}
+      />
+    </div>
+  );
 }
 
-function PartyTableRows(props) {
-  rp(props.link)
-    .then(html => console.log(html))
-  console.log('blah')
+function PartyTableRow(props) {
   return(
     <Table.Row>
-      <Table.Cell>1</Table.Cell>
-      <Table.Cell>2</Table.Cell>
-      <Table.Cell>3</Table.Cell>
-      <Table.Cell>4</Table.Cell>
+      <Table.Cell>{props.id}</Table.Cell>
+      <Table.Cell>{props.title}</Table.Cell>
+      <Table.Cell>{props.start}</Table.Cell>
+      <Table.Cell>{props.end}</Table.Cell>
     </Table.Row>
   );
 }
@@ -65,13 +57,32 @@ class PartyTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      link: props.link,
+      rows:null,
     }
-    console.log(this.state.link);
   }
 
-  generateTable() {
-    if(this.state.link){
+  render() {
+    const reports = this.props.reports;
+
+    if(reports){
+      var rows = reports.data.map((report,number) => {
+        console.log(report.id);
+        return (
+          <PartyTableRow
+            key={report.id}
+            id={report.id}
+            title={report.title}
+            start={report.start}
+            end={report.end}
+          />
+        )
+      });
+    }
+    else {
+      rows = <PartyTableRow/>;
+    }
+
+    if(this.props.link){
       return (
         <Container>
           <Table celled>
@@ -83,9 +94,9 @@ class PartyTable extends React.Component {
                 <Table.HeaderCell>Actions</Table.HeaderCell>
               </Table.Row>
             </Table.Header>
-            <PartyTableRows
-              link={this.state.link}
-            />
+            <Table.Body>
+              {rows}
+            </Table.Body>
           </Table>
         </Container>
       );
@@ -100,12 +111,6 @@ class PartyTable extends React.Component {
       );
     }
   }
-
-  render() {
-    return (
-      this.generateTable()
-    );
-  }
 }
 
 class PartyCheck extends React.Component {
@@ -113,6 +118,7 @@ class PartyCheck extends React.Component {
     super(props);
     this.state = {
       link: null,
+      reports: null,
     }
 
     this.checkAndGo = this.checkAndGo.bind(this);
@@ -123,11 +129,28 @@ class PartyCheck extends React.Component {
   }
 
   checkAndGo(i) {
-    if (i.target.value === 'https://www.fflogs.com/user/reports-list/237412/')
+    if (i.target.value === 'TheAlpacalypse')
     {
       this.setState({
         link: i.target.value,
       });
+
+      console.log(this.state.link);
+      const query = format("https://www.fflogs.com/v1/reports/user/{username}?api_key={api_key}", {
+        username:i.target.value,
+        // api_key:process.env.API_KEY
+        api_key:''
+      });
+
+      console.log(query);
+
+      axios.get(query)
+        .then((response) => {
+          console.log(response);
+          this.setState({
+            reports:response,
+          });
+        });
     }
     return;
   }
@@ -150,6 +173,7 @@ class PartyCheck extends React.Component {
         <br></br>
         <PartyTable
           link={this.state.link}
+          reports={this.state.reports}
         />
       </div>
     );
