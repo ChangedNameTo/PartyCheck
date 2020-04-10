1310c3fe9956a3a9d49aed35b4b61a6a972eb26d
Will Mitchell <william.m95@gmail.com>
2020-04-10 09:36:43 -0400

    Baseline testing WIP

--- cypress/integration/party_check.spec.js
+++ cypress/integration/party_check.spec.js
@@ -1,5 +1,24 @@
-describe('My First Test', () => {
-  it('Opens PartyCheck', () => {
-    cy.visit('localhost:3000')
+describe('Baseline Functionality Testing', () => {
+  it('Opens PartyCheck and checks for the Welcome', () => {
+    cy.contains('Welcome to PartyCheck!');
+  });
+
+  it('Opens PartyCheck, clicks Search, and nothing happens', () => {
+    cy.contains('Search').click();
+    cy.contains('Welcome to PartyCheck!');
+  });
+
+  it('Should type `TheAlpacalypse` into the search and see the results table', () => {
+    cy.get('input').type('TheAlpacalypse')
+    cy.contains('Search').click();
+    cy.get('table')
   })
+
+  it('Should type `The` into the search and see the error warning', () => {
+    cy.get('input').type('The')
+    cy.contains('Search').click();
+    cy.contains('You need to enter a valid FFLogs username.')
+  })
+
+  // it('Should get a valid JSON response and then show Fights')
 })
